4cbb6253f1332a806b7a1101b7f64e17ee81f82d
ousttrue <ousttrue@gmail.com>
2022-05-03 22:04:29 +0900

    use pickle

--- src/pydear/utils/node_editor.py
+++ src/pydear/utils/node_editor.py
@@ -6,19 +6,6 @@ from pydear import imnodes as ImNodes
 from pydear.utils.setting import BinSetting
 
 
-class IdGenerator:
-    def __init__(self) -> None:
-        self.next_id = 1
-
-    def __call__(self) -> int:
-        value = self.next_id
-        self.next_id += 1
-        return value
-
-
-NEXT_ID = IdGenerator()
-
-
 class InputPinData(TypedDict):
     id: int
     name: str
@@ -37,13 +24,9 @@ class NodeData(TypedDict):
 
 
 class InputPin:
-    def __init__(self, name: str, *, id=None) -> None:
-        self.id = id if isinstance(id, int) else NEXT_ID()
+    def __init__(self, id: int, name: str) -> None:
+        self.id = id
         self.name = name
-
-    @staticmethod
-    def load(value: InputPinData) -> 'InputPin':
-        return InputPin(value['name'], id=value['id'])
 
     def show(self):
         ImNodes.BeginInputAttribute(self.id)
@@ -52,13 +35,9 @@ class InputPin:
 
 
 class OutputPin:
-    def __init__(self, name: str, *, id=None) -> None:
-        self.id = id if isinstance(id, int) else NEXT_ID()
+    def __init__(self, id: int, name: str) -> None:
+        self.id = id
         self.name = name
-
-    @staticmethod
-    def load(value: OutputPinData) -> 'OutputPin':
-        return OutputPin(value['name'], id=value['id'])
 
     def show(self):
         ImNodes.BeginOutputAttribute(self.id)
@@ -68,28 +47,11 @@ class OutputPin:
 
 
 class Node:
-    def __init__(self, title: str, inputs: List[InputPin], outputs: List[OutputPin], *, id=None) -> None:
-        self.id = id if isinstance(id, int) else NEXT_ID()
+    def __init__(self, id: int, title: str, inputs: List[InputPin], outputs: List[OutputPin]) -> None:
+        self.id = id
         self.title = title
         self.inputs = inputs
         self.outputs = outputs
-
-    @staticmethod
-    def load(value: NodeData) -> 'Node':
-        return Node(
-            value['title'],
-            id=value['id'],
-            inputs=[InputPin.load(x) for x in value['inputs']],
-            outputs=[OutputPin.load(x) for x in value['outputs']],
-        )
-
-    def dump(self) -> NodeData:
-        return NodeData(
-            id=self.id,
-            title=self.title,
-            inputs=[InputPinData(id=x.id, name=x.name) for x in self.inputs],
-            outputs=[OutputPinData(id=x.id, name=x.name) for x in self.outputs]
-        )
 
     def contains(self, link: Tuple[int, int]) -> bool:
         for input in self.inputs:
@@ -99,6 +61,26 @@ class Node:
             if output.id in link:
                 return True
         return False
+
+    def show(self):
+        ImNodes.BeginNode(self.id)
+
+        ImNodes.BeginNodeTitleBar()
+        ImGui.TextUnformatted(self.title)
+        ImNodes.EndNodeTitleBar()
+
+        self.show_content()
+
+        for input in self.inputs:
+            input.show()
+
+        for output in self.outputs:
+            output.show()
+
+        ImNodes.EndNode()
+
+    def show_content(self):
+        pass
 
 
 # class InputPin:
@@ -171,24 +153,16 @@ SETTING_KEY = 'imnodes'
 SETTING_GRAPH_KEY = 'imnodes_graph'
 
 
-class NodeEditor:
-    '''
-    TODO: undo, redo
-    '''
-
-    def __init__(self, name: str, *, setting: Optional[BinSetting] = None) -> None:
-        self.settting = setting
-        self.name = name
-        self.is_initialized = False
+class Graph:
+    def __init__(self) -> None:
+        self.next_id = 1
         self.nodes: List[Node] = []
         self.links: List[Tuple[int, int]] = []
-        self.start_attr = (ctypes.c_int * 1)()
-        self.end_attr = (ctypes.c_int * 1)()
 
-    def __del__(self):
-        if self.is_initialized:
-            ImNodes.DestroyContext()
-            self.is_initialized = False
+    def get_next_id(self) -> int:
+        value = self.next_id
+        self.next_id += 1
+        return value
 
     def remove_link(self, node: Node):
         self.links = [link for link in self.links if not node.contains(link)]
@@ -201,14 +175,33 @@ class NodeEditor:
                 break
 
         if not self.nodes:
-            NEXT_ID.next_id = 1
+            self.next_id = 1
+
+
+class NodeEditor:
+    '''
+    TODO: undo, redo
+    '''
+
+    def __init__(self, name: str, *, setting: Optional[BinSetting] = None) -> None:
+        self.settting = setting
+        self.name = name
+        self.is_initialized = False
+        self.start_attr = (ctypes.c_int * 1)()
+        self.end_attr = (ctypes.c_int * 1)()
+        self.graph = Graph()
+
+    def __del__(self):
+        if self.is_initialized:
+            ImNodes.DestroyContext()
+            self.is_initialized = False
 
     def save(self):
         if self.settting:
             self.settting[SETTING_KEY] = ImNodes.SaveCurrentEditorStateToIniString().encode(
                 'utf-8')
-            self.settting[SETTING_GRAPH_KEY] = json.dumps(
-                self.dump_graph()).encode('utf-8')
+            import pickle
+            self.settting[SETTING_GRAPH_KEY] = pickle.dumps(self.graph)
 
     def load(self):
         if self.settting:
@@ -217,19 +210,11 @@ class NodeEditor:
                 ImNodes.LoadCurrentEditorStateFromIniString(data, len(data))
             graph_data = self.settting[SETTING_GRAPH_KEY]
             if graph_data:
-                graph = json.loads(graph_data)
-                for node in graph.get('nodes', []):
-                    self.nodes.append(Node.load(node))
-                for link in graph.get('links', []):
-                    self.links.append(link)
-                NEXT_ID.next_id = graph.get('next_id', 1)
-
-    def dump_graph(self):
-        return {
-            'nodes': [node.dump() for node in self.nodes],
-            'links': self.links,
-            'next_id': NEXT_ID.next_id,
-        }
+                try:
+                    import pickle
+                    self.graph = pickle.loads(graph_data)
+                except:
+                    self.graph = Graph()
 
     def before_node_editor(self):
         '''
@@ -254,33 +239,39 @@ class NodeEditor:
 
         if ImGui.BeginPopup("add node"):
             click_pos = ImGui.GetMousePosOnOpeningCurrentPopup()
+            next_id = self.graph.get_next_id
             if ImGui.MenuItem("add"):
-                node = Node('add',
-                            [InputPin('a'), InputPin('b')],
-                            [OutputPin('value')])
-                self.nodes.append(node)
+                node = Node(next_id(), 'add',
+                            [InputPin(next_id(), 'a'),
+                             InputPin(next_id(), 'b')],
+                            [OutputPin(next_id(), 'value')])
+                self.graph.nodes.append(node)
                 ImNodes.SetNodeScreenSpacePos(node.id, click_pos)
 
             if ImGui.MenuItem("multiply"):
-                node = Node('mult',
-                            [InputPin('a'), InputPin('b')],
-                            [OutputPin('value')])
-                self.nodes.append(node)
+                node = Node(next_id(), 'mult',
+                            [InputPin(next_id(), 'a'),
+                             InputPin(next_id(), 'b')],
+                            [OutputPin(next_id(), 'value')])
+                self.graph.nodes.append(node)
                 ImNodes.SetNodeScreenSpacePos(node.id, click_pos)
 
             if ImGui.MenuItem("output"):
-                node = Node('output', [InputPin('value')], [])
-                self.nodes.append(node)
+                node = Node(next_id(), 'output', [
+                            InputPin(next_id(), 'value')], [])
+                self.graph.nodes.append(node)
                 ImNodes.SetNodeScreenSpacePos(node.id, click_pos)
 
             if ImGui.MenuItem("sine"):
-                node = Node('sine', [], [OutputPin('value')])
-                self.nodes.append(node)
+                node = Node(next_id(), 'sine', [], [
+                            OutputPin(next_id(), 'value')])
+                self.graph.nodes.append(node)
                 ImNodes.SetNodeScreenSpacePos(node.id, click_pos)
 
             if ImGui.MenuItem("time"):
-                node = Node('time', [], [OutputPin('value')])
-                self.nodes.append(node)
+                node = Node(next_id(), 'time', [], [
+                            OutputPin(next_id(), 'value')])
+                self.graph.nodes.append(node)
                 ImNodes.SetNodeScreenSpacePos(node.id, click_pos)
 
             ImGui.EndPopup()
@@ -305,29 +296,29 @@ class NodeEditor:
 
             self.on_node_editor()
 
-            for node in self.nodes:
-                self.show_node(node)
+            for node in self.graph.nodes:
+                node.show()
 
-            for i, (begin, end) in enumerate(self.links):
+            for i, (begin, end) in enumerate(self.graph.links):
                 ImNodes.Link(i, begin, end)
 
             ImNodes.EndNodeEditor()
 
             if ImNodes.IsLinkCreated(self.start_attr, self.end_attr):
                 # add link
-                self.links.append((self.start_attr[0], self.end_attr[0]))
+                self.graph.links.append((self.start_attr[0], self.end_attr[0]))
 
             if ImNodes.IsLinkDestroyed(self.start_attr):
                 # remove unlink
-                del self.links[self.start_attr[0]]
+                del self.graph.links[self.start_attr[0]]
 
             num_selected = ImNodes.NumSelectedLinks()
             if num_selected and ImGui.IsKeyPressed(ImGui.ImGuiKey_.X):
                 # remove selected link
                 selected_links = (ctypes.c_int * num_selected)()
                 ImNodes.GetSelectedLinks(selected_links)
-                self.links = [link for i, link in enumerate(
-                    self.links) if (i not in selected_links)]
+                self.graph.links = [link for i, link in enumerate(
+                    self.graph.links) if (i not in selected_links)]
 
             num_selected = ImNodes.NumSelectedNodes()
             if num_selected and ImGui.IsKeyPressed(ImGui.ImGuiKey_.X):
@@ -335,26 +326,6 @@ class NodeEditor:
                 selected_nodes = (ctypes.c_int * num_selected)()
                 ImNodes.GetSelectedNodes(selected_nodes)
                 for node_id in selected_nodes:
-                    self.remove_node(node_id)
+                    self.graph.remove_node(node_id)
 
         ImGui.End()
-
-    def show_node(self, node: Node):
-        ImNodes.BeginNode(node.id)
-
-        ImNodes.BeginNodeTitleBar()
-        ImGui.TextUnformatted(node.title)
-        ImNodes.EndNodeTitleBar()
-
-        self.show_node_content(node)
-
-        for input in node.inputs:
-            input.show()
-
-        for output in node.outputs:
-            output.show()
-
-        ImNodes.EndNode()
-
-    def show_node_content(self, node: Node):
-        pass
