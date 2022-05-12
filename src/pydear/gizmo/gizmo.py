d5285360d3d4c791675e51d73a038547ce9a1b1d
ousttrue <ousttrue@gmail.com>
2022-05-13 01:26:32 +0900

    Gizmo only hover handling

--- src/pydear/gizmo/gizmo.py
+++ src/pydear/gizmo/gizmo.py
@@ -1,95 +1,24 @@
-from typing import Optional, Dict, List
+from lib2to3.pgen2.token import OP
+from typing import Optional, Dict, List, NamedTuple
 import glm
-from pydear.scene.camera import Camera
-from pydear.utils.mouse_event import MouseEvent
-from pydear.utils.eventproperty import EventProperty
+from pydear.scene.camera import Camera, Ray
 from .shapes.shape import Shape, ShapeState
-from .shapes.cube_shape import CubeShape
-from .shapes.ring_shape import RingShape
 from .triangle_buffer import TriangleBuffer
 
 
-class DragContext:
-    def __init__(self, x, y, *, manipulator: Shape, selected: Shape) -> None:
-        self.manipulator = manipulator
-        self.manipulator.add_state(ShapeState.DRAG)
-        assert selected
-        self.selected = selected
-        self.x = x
-        self.y = y
-        self.init_matrix = selected.matrix.value
-
-    def drag(self, x, y, dx, dy):
-        angle = (y - self.y) * 0.02
-        self.selected.matrix.set(
-            self.init_matrix * glm.rotate(angle, glm.vec3(0, 0, 1)))
-
-    def end(self):
-        self.manipulator.remove_state(ShapeState.DRAG)
+class RayHit(NamedTuple):
+    shape: Optional[Shape]
+    x: int
+    y: int
+    ray: Ray
+    distance: float
 
 
 class Gizmo:
-    '''
-    [triangles] の登録
-    * 1weight skinning
-    * cube
-    * ring
-    * bone
-    * rgba
-    * normal diffuse + ambient
-
-    [lines] の登録
-    * 1weight skinning
-    * axis
-    * rgba
-
-    [mouse event]
-    cursor ray の[triangles]に対するあたり判定 => hover(highlight)
-    hover に対する click(selector)/drag(manipulator) 
-    '''
-
     def __init__(self) -> None:
         self.vertex_buffer = TriangleBuffer()
-        self.mouse_event = None
         self.shapes: List[Shape] = []
-        self.selected: EventProperty[Optional[Shape]] = EventProperty(None)
-        self.hover: Optional[Shape] = None
-        self.drag_context = None
-
-    def bind_mouse_event(self, mouse_event: MouseEvent):
-        '''
-        use left mouse
-        '''
-        self.mouse_event = mouse_event
-        mouse_event.left_pressed.append(self.drag_begin)
-        mouse_event.left_drag.append(self.drag)
-        mouse_event.left_released.append(self.drag_end)
-
-    def drag_begin(self, x, y):
-        if self.hover:
-            if self.hover.is_draggable:
-                self.drag_context = DragContext(x, y,
-                                                manipulator=self.hover,
-                                                selected=self.selected.value)
-            else:
-                if self.hover != self.selected.value:
-                    if selected := self.selected.value:
-                        selected.remove_state(ShapeState.SELECT)
-                    self.selected.set(self.hover)
-                    self.hover.add_state(ShapeState.SELECT)
-        else:
-            if selected := self.selected.value:
-                selected.remove_state(ShapeState.SELECT)
-                self.selected.set(None)
-
-    def drag(self, x, y, dx, dy):
-        if self.drag_context:
-            self.drag_context.drag(x, y, dx, dy)
-
-    def drag_end(self, x, y):
-        if self.drag_context:
-            self.drag_context.end()
-            self.drag_context = None
+        self.hit = RayHit(None, 0, 0, Ray(glm.vec3(), glm.vec3()), float('inf'))
 
     def add_shape(self, shape: Shape) -> int:
         key = len(self.shapes)
@@ -105,18 +34,20 @@ class Gizmo:
         # ray intersect
         ray = camera.get_mouse_ray(x, y)
         hit_shape = None
-        hit_distance = 0
+        hit_distance = float('inf')
         for i, shape in enumerate(self.shapes):
             distance = shape.intersect(ray)
             if distance:
-                if (not hit_shape) or (distance < hit_distance):
+                if distance < hit_distance:
                     hit_shape = shape
                     hit_distance = distance
 
         # update hover
-        if hit_shape != self.hover:
-            if self.hover:
-                self.hover.remove_state(ShapeState.HOVER)
-            self.hover = hit_shape
-            if self.hover:
-                self.hover.add_state(ShapeState.HOVER)
+        hover_shape = self.hit.shape if self.hit else None
+        if hit_shape != hover_shape:
+            if hover_shape:
+                hover_shape.remove_state(ShapeState.HOVER)
+
+        self.hit = RayHit(hit_shape, x, y, ray, hit_distance)
+        if self.hit.shape:
+            self.hit.shape.add_state(ShapeState.HOVER)
