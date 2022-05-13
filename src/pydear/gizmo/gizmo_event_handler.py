4fbb7eeca562900e493feb55d53c7af664d7c5d4
ousttrue <ousttrue@gmail.com>
2022-05-13 16:43:08 +0900

    xyz

--- src/pydear/gizmo/gizmo_event_handler.py
+++ src/pydear/gizmo/gizmo_event_handler.py
@@ -54,20 +54,21 @@ class GizmoSelectHandler(GizmoEventHandler):
 
 
 class DragContext:
-    def __init__(self, x, y, manipulator, target: Shape):
+    def __init__(self, x, y, *, manipulator, axis, target: Shape):
         self.y = y
         assert(manipulator)
         self.manipulator = manipulator
         self.manipulator.add_state(ShapeState.DRAG)
+        self.axis = axis
         assert target
         self.target = target
         self.init_matrix = target.matrix.value
 
     def drag(self, x: int, y: int):
         angle = (y - self.y) * 0.02
-        if self.target:
-            self.target.matrix.set(
-                self.init_matrix * glm.rotate(angle, glm.vec3(0, 0, 1)))
+        m = self.init_matrix * glm.rotate(angle, self.axis)
+        self.target.matrix.set(m)
+        return m
 
     def end(self):
         self.manipulator.remove_state(ShapeState.DRAG)
@@ -80,24 +81,42 @@ class DragEventHandler(GizmoEventHandler):
         self.selected = EventProperty[Optional[Shape]](None)
 
         # draggable
-        from pydear.gizmo.shapes.ring_shape import RingShape
-        self.ring = RingShape(inner=0.4, outer=0.6, depth=0.02,
-                              color=glm.vec4(0.3, 0.3, 1, 1))
-        gizmo.add_shape(self.ring)
+        from pydear.gizmo.shapes.ring_shape import XRingShape, YRingShape, ZRingShape
+        self.x_ring = XRingShape(inner=0.4, outer=0.6, depth=0.02,
+                                 color=glm.vec4(1, 0.3, 0.3, 1))
+        gizmo.add_shape(self.x_ring)
+        self.y_ring = YRingShape(inner=0.4, outer=0.6, depth=0.02,
+                                 color=glm.vec4(0.3, 1, 0.3, 1))
+        gizmo.add_shape(self.y_ring)
+        self.z_ring = ZRingShape(inner=0.4, outer=0.6, depth=0.02,
+                                 color=glm.vec4(0.3, 0.3, 1, 1))
+        gizmo.add_shape(self.z_ring)
 
         self.context = None
 
     def drag_begin(self, hit: RayHit):
-        if hit.shape == self.ring:
-            # ring is not selectable
-            self.context = DragContext(
-                hit.x, hit.y, hit.shape, self.selected.value)
-        else:
-            self.select(hit)
+        match hit.shape:
+            case self.x_ring:
+                # ring is not selectable
+                self.context = DragContext(
+                    hit.x, hit.y, manipulator=hit.shape, axis=glm.vec3(1, 0, 0), target=self.selected.value)
+            case self.y_ring:
+                # ring is not selectable
+                self.context = DragContext(
+                    hit.x, hit.y, manipulator=hit.shape, axis=glm.vec3(0, 1, 0), target=self.selected.value)
+            case self.z_ring:
+                # ring is not selectable
+                self.context = DragContext(
+                    hit.x, hit.y, manipulator=hit.shape, axis=glm.vec3(0, 0, 1), target=self.selected.value)
+            case _:
+                self.select(hit)
 
     def drag(self, x, y, dx, dy):
         if self.context:
-            self.context.drag(x, y)
+            m = self.context.drag(x, y)
+            self.x_ring.matrix.set(m)
+            self.y_ring.matrix.set(m)
+            self.z_ring.matrix.set(m)
 
     def drag_end(self, x, y):
         if self.context:
@@ -114,7 +133,13 @@ class DragEventHandler(GizmoEventHandler):
             self.selected.set(shape)
             if shape:
                 shape.add_state(ShapeState.SELECT)
-                self.ring.matrix.set(shape.matrix.value)
-                self.ring.remove_state(ShapeState.HIDE)
+                self.x_ring.matrix.set(shape.matrix.value)
+                self.x_ring.remove_state(ShapeState.HIDE)
+                self.y_ring.matrix.set(shape.matrix.value)
+                self.y_ring.remove_state(ShapeState.HIDE)
+                self.z_ring.matrix.set(shape.matrix.value)
+                self.z_ring.remove_state(ShapeState.HIDE)
             else:
-                self.ring.add_state(ShapeState.HIDE)
+                self.x_ring.add_state(ShapeState.HIDE)
+                self.y_ring.add_state(ShapeState.HIDE)
+                self.z_ring.add_state(ShapeState.HIDE)
--- src/pydear/gizmo/shapes/ring_shape.py
+++ src/pydear/gizmo/shapes/ring_shape.py
@@ -5,7 +5,93 @@ from ..primitive import Quad
 from .shape import Shape, ShapeState
 
 
-class RingShape(Shape):
+class XRingShape(Shape):
+    def __init__(self, *, inner: float, outer: float, depth: float, theta: float = math.pi * 2, sections=20, color=None) -> None:
+        super().__init__(glm.mat4(0), True)
+        self.state.set(ShapeState.HIDE)
+        self.color = color if color else glm.vec4(1, 1, 1, 1)
+        delta = theta/sections
+        angle = 0
+        angles = []
+        for i in range(sections):
+            angles.append(angle)
+            angle += delta
+        sin_cos = [(math.sin(angle), math.cos(angle)) for angle in angles]
+        self.quads = []
+
+        d = depth * 0.5
+        for i, (s0, c0) in enumerate(sin_cos):
+            s1, c1 = sin_cos[(i+1) % sections]
+            v0 = glm.vec3(d, c0 * inner, s0 * inner)
+            v1 = glm.vec3(d, c0 * outer, s0 * outer)
+            v2 = glm.vec3(d, c1 * outer, s1 * outer)
+            v3 = glm.vec3(d, c1 * inner, s1 * inner)
+            v4 = glm.vec3(-d, c0 * inner, s0 * inner)
+            v5 = glm.vec3(-d, c0 * outer, s0 * outer)
+            v6 = glm.vec3(-d, c1 * outer, s1 * outer)
+            v7 = glm.vec3(-d, c1 * inner, s1 * inner)
+            '''
+                  v7 v6
+            v3 v2 v4 v5
+            v0 v1
+            '''
+            self.quads += [
+                Quad.from_points(v0, v1, v2, v3),
+                Quad.from_points(v1, v5, v6, v2),
+                Quad.from_points(v7, v6, v5, v4),
+            ]
+
+    def get_color(self) -> glm.vec4:
+        return self.color
+
+    def get_quads(self) -> Iterable[Quad]:
+        return self.quads
+
+
+class YRingShape(Shape):
+    def __init__(self, *, inner: float, outer: float, depth: float, theta: float = math.pi * 2, sections=20, color=None) -> None:
+        super().__init__(glm.mat4(0), True)
+        self.state.set(ShapeState.HIDE)
+        self.color = color if color else glm.vec4(1, 1, 1, 1)
+        delta = theta/sections
+        angle = 0
+        angles = []
+        for i in range(sections):
+            angles.append(angle)
+            angle += delta
+        sin_cos = [(math.sin(angle), math.cos(angle)) for angle in angles]
+        self.quads = []
+
+        d = depth * 0.5
+        for i, (s0, c0) in enumerate(sin_cos):
+            s1, c1 = sin_cos[(i+1) % sections]
+            v0 = glm.vec3(s0 * inner, d, c0 * inner)
+            v1 = glm.vec3(s0 * outer, d, c0 * outer)
+            v2 = glm.vec3(s1 * outer, d, c1 * outer)
+            v3 = glm.vec3(s1 * inner, d, c1 * inner)
+            v4 = glm.vec3(s0 * inner, -d, c0 * inner)
+            v5 = glm.vec3(s0 * outer, -d, c0 * outer)
+            v6 = glm.vec3(s1 * outer, -d, c1 * outer)
+            v7 = glm.vec3(s1 * inner, -d, c1 * inner)
+            '''
+                  v7 v6
+            v3 v2 v4 v5
+            v0 v1
+            '''
+            self.quads += [
+                Quad.from_points(v0, v1, v2, v3),
+                Quad.from_points(v1, v5, v6, v2),
+                Quad.from_points(v7, v6, v5, v4),
+            ]
+
+    def get_color(self) -> glm.vec4:
+        return self.color
+
+    def get_quads(self) -> Iterable[Quad]:
+        return self.quads
+
+
+class ZRingShape(Shape):
     def __init__(self, *, inner: float, outer: float, depth: float, theta: float = math.pi * 2, sections=20, color=None) -> None:
         super().__init__(glm.mat4(0), True)
         self.state.set(ShapeState.HIDE)
