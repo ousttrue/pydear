fdc02038b47a8402b4272cf1942a352719b3491f
ousttrue <ousttrue@gmail.com>
2022-05-17 02:00:21 +0900

    RollShape

--- src/pydear/gizmo/gizmo_drag_handler.py
+++ src/pydear/gizmo/gizmo_drag_handler.py
@@ -31,16 +31,33 @@ class ScreenLine:
         return glm.vec2(x, self.a * x - self.b)
 
 
+
+# ScreenSplitDragger
+
+
 class DragContext:
-    def __init__(self, start_screen_pos: glm.vec2, *, manipulator: Shape, axis: Axis, target: Shape, camera: Camera):
+    def __init__(self, start_screen_pos: glm.vec2, *, manipulator: Shape, axis: Axis, target: Shape) -> None:
         self.start_screen_pos = start_screen_pos
-        assert(manipulator)
         self.manipulator = manipulator
+        assert(self.manipulator)
         self.manipulator.add_state(ShapeState.DRAG)
         self.axis = axis
         assert target
         self.target = target
         self.init_matrix = target.matrix.value
+
+    def end(self):
+        self.manipulator.remove_state(ShapeState.DRAG)
+        self.manipulator = None
+
+
+class RingDragContext(DragContext):
+    '''
+    円盤面のドラッグ
+    '''
+
+    def __init__(self, start_screen_pos: glm.vec2, *, manipulator: Shape, axis: Axis, target: Shape, camera: Camera):
+        super().__init__(start_screen_pos, manipulator=manipulator, axis=axis, target=target)
 
         vp = camera.projection.matrix * camera.view.matrix
         center_pos = vp * manipulator.matrix.value[3]
@@ -60,43 +77,46 @@ class DragContext:
         center_start = glm.vec3(self.start_screen_pos, 0) - \
             glm.vec3(self.center_screen_pos, 0)
         self.cross = glm.normalize(glm.cross(center_start, view_axis))
-        if self.cross.x == 0 and self.cross.y == 0:
-            self.edge = True
-            cs = self.center_screen_pos - self.start_screen_pos
-            if self.cross.z > 0:
-                self.cross = glm.normalize(glm.vec3(cs.x, cs.y, 0))
-            else:
-                self.cross = -glm.normalize(glm.vec3(cs.x, cs.y, 0))
-        else:
-            self.edge = False
-            self.cross.z = 0
-            self.cross = glm.normalize(self.cross)
+        assert self.cross.x != 0 or self.cross.y != 0
+        self.edge = False
+        self.cross.z = 0
+        self.cross = glm.normalize(self.cross)
 
     def drag(self, cursor_pos: glm.vec2):
-        # d = cursor_pos.y - self.start_screen_pos.y
-
+        # TODO: center project to screen
+        # TODO: split by center-start
         start_cursor = glm.vec3(cursor_pos, 0) - \
             glm.vec3(self.start_screen_pos, 0)
         d = glm.dot(self.cross, start_cursor)
         angle = d * 0.02
-
         m = self.init_matrix * glm.rotate(angle, IDENTITY[self.axis.value].xyz)
         self.target.matrix.set(m)
         return m
 
-    def end(self):
-        self.manipulator.remove_state(ShapeState.DRAG)
-        self.manipulator = None
+
+class RollgDragContext(DragContext):
+    '''
+    横面のドラッグ
+    '''
+
+    def __init__(self, start_screen_pos: glm.vec2, *, manipulator: Shape, axis: Axis, target: Shape, camera: Camera):
+        super().__init__(start_screen_pos, manipulator=manipulator, axis=axis, target=target)
+
+    def drag(self, cursor_pos: glm.vec2):
+        # TODO: axis project to screen
+        # TODO: split by start-axis_dir
+        m = self.init_matrix
+        return m
 
 
 class GizmoDragHandler(GizmoEventHandler):
-    def __init__(self, gizmo: Gizmo, camera, *, inner=0.4, outer=0.6, depth=0.02) -> None:
+    def __init__(self, gizmo: Gizmo, camera, *, inner=0.4, outer=0.6, depth=0.04) -> None:
         super().__init__()
         self.camera = camera
         self.selected = EventProperty[Optional[Shape]](None)
 
         # draggable
-        from pydear.gizmo.shapes.ring_shape import XRingShape, YRingShape, ZRingShape
+        from pydear.gizmo.shapes.ring_shape import XRingShape, YRingShape, ZRingShape, XRollShape, YRollShape, ZRollShape
         self.x_ring = XRingShape(inner=inner, outer=outer, depth=depth,
                                  color=glm.vec4(1, 0.3, 0.3, 1))
         gizmo.add_shape(self.x_ring)
@@ -107,23 +127,33 @@ class GizmoDragHandler(GizmoEventHandler):
                                  color=glm.vec4(0.3, 0.3, 1, 1))
         gizmo.add_shape(self.z_ring)
 
+        self.x_roll = XRollShape(inner=inner, outer=outer, depth=depth,
+                                 color=glm.vec4(1, 0.3, 0.3, 1))
+        gizmo.add_shape(self.x_roll)
+        self.y_roll = YRollShape(inner=inner, outer=outer, depth=depth,
+                                 color=glm.vec4(0.3, 1, 0.3, 1))
+        gizmo.add_shape(self.y_roll)
+        self.z_roll = ZRollShape(inner=inner, outer=outer, depth=depth,
+                                 color=glm.vec4(0.3, 0.3, 1, 1))
+        gizmo.add_shape(self.z_roll)
+
         self.context = None
 
     def drag_begin(self, hit: RayHit):
         match hit.shape:
             case self.x_ring:
                 # ring is not selectable
-                self.context = DragContext(
+                self.context = RingDragContext(
                     hit.cursor_pos, manipulator=hit.shape, axis=Axis.X,
                     target=self.selected.value, camera=self.camera)
             case self.y_ring:
                 # ring is not selectable
-                self.context = DragContext(
+                self.context = RingDragContext(
                     hit.cursor_pos, manipulator=hit.shape, axis=Axis.Y,
                     target=self.selected.value, camera=self.camera)
             case self.z_ring:
                 # ring is not selectable
-                self.context = DragContext(
+                self.context = RingDragContext(
                     hit.cursor_pos, manipulator=hit.shape, axis=Axis.Z,
                     target=self.selected.value, camera=self.camera)
             case _:
@@ -135,6 +165,9 @@ class GizmoDragHandler(GizmoEventHandler):
             self.x_ring.matrix.set(m)
             self.y_ring.matrix.set(m)
             self.z_ring.matrix.set(m)
+            self.x_roll.matrix.set(m)
+            self.y_roll.matrix.set(m)
+            self.z_roll.matrix.set(m)
 
     def drag_end(self, x, y):
         if self.context:
@@ -157,7 +190,16 @@ class GizmoDragHandler(GizmoEventHandler):
             self.y_ring.remove_state(ShapeState.HIDE)
             self.z_ring.matrix.set(shape.matrix.value)
             self.z_ring.remove_state(ShapeState.HIDE)
+            self.x_roll.matrix.set(shape.matrix.value)
+            self.x_roll.remove_state(ShapeState.HIDE)
+            self.y_roll.matrix.set(shape.matrix.value)
+            self.y_roll.remove_state(ShapeState.HIDE)
+            self.z_roll.matrix.set(shape.matrix.value)
+            self.z_roll.remove_state(ShapeState.HIDE)
         else:
             self.x_ring.add_state(ShapeState.HIDE)
             self.y_ring.add_state(ShapeState.HIDE)
             self.z_ring.add_state(ShapeState.HIDE)
+            self.x_roll.add_state(ShapeState.HIDE)
+            self.y_roll.add_state(ShapeState.HIDE)
+            self.z_roll.add_state(ShapeState.HIDE)
--- src/pydear/gizmo/shapes/ring_shape.py
+++ src/pydear/gizmo/shapes/ring_shape.py
@@ -36,7 +36,7 @@ class RingShape(Shape):
             '''
             self.quads += [
                 Quad.from_points(v0, v1, v2, v3),
-                Quad.from_points(v1, v5, v6, v2),
+                # Quad.from_points(v1, v5, v6, v2),
                 Quad.from_points(v7, v6, v5, v4),
             ]
 
@@ -63,6 +63,70 @@ class YRingShape(RingShape):
 
 
 class ZRingShape(RingShape):
+    def __init__(self, *, inner: float, outer: float, depth: float, theta: float = math.pi * 2, sections=20, color=None) -> None:
+        super().__init__(axis=glm.vec3(0, 0, 1), start=glm.vec3(1, 0, 0),
+                         inner=inner, outer=outer,
+                         depth=depth, theta=theta, sections=sections, color=color)
+
+
+class RollShape(Shape):
+    def __init__(self, *, axis: glm.vec3, start: glm.vec3, inner: float, outer: float, depth: float, theta: float = math.pi * 2, sections=20, color=None) -> None:
+        super().__init__(glm.mat4(0))
+        self.state.set(ShapeState.HIDE)
+        self.color = color if color else glm.vec4(1, 1, 1, 1)
+        delta = theta/sections
+        angle = 0
+        vertices = []
+        for i in range(sections):
+            vertices.append(glm.angleAxis(angle, axis) * start)
+            angle += delta
+        self.quads = []
+
+        d = axis * depth * 0.5
+        for i, v in enumerate(vertices):
+            vv = vertices[(i+1) % sections]
+            v0 = d + v * inner
+            v1 = d + v * outer
+            v2 = d + vv * outer
+            v3 = d + vv * inner
+            v4 = -d + v * inner
+            v5 = -d + v * outer
+            v6 = -d + vv * outer
+            v7 = -d + vv * inner
+            '''
+                  v7 v6
+            v3 v2 v4 v5
+            v0 v1
+            '''
+            self.quads += [
+                # Quad.from_points(v0, v1, v2, v3),
+                Quad.from_points(v1, v5, v6, v2),
+                # Quad.from_points(v7, v6, v5, v4),
+            ]
+
+    def get_quads(self) -> Iterable[Tuple[Quad, glm.vec4]]:
+        for quad in self.quads:
+            yield quad, self.color
+
+    def get_lines(self):
+        return []
+
+
+class XRollShape(RollShape):
+    def __init__(self, *, inner: float, outer: float, depth: float, theta: float = math.pi * 2, sections=20, color=None) -> None:
+        super().__init__(axis=glm.vec3(1, 0, 0), start=glm.vec3(0, 1, 0),
+                         inner=inner, outer=outer,
+                         depth=depth, theta=theta, sections=sections, color=color)
+
+
+class YRollShape(RollShape):
+    def __init__(self, *, inner: float, outer: float, depth: float, theta: float = math.pi * 2, sections=20, color=None) -> None:
+        super().__init__(axis=glm.vec3(0, 1, 0), start=glm.vec3(0, 0, 1),
+                         inner=inner, outer=outer,
+                         depth=depth, theta=theta, sections=sections, color=color)
+
+
+class ZRollShape(RollShape):
     def __init__(self, *, inner: float, outer: float, depth: float, theta: float = math.pi * 2, sections=20, color=None) -> None:
         super().__init__(axis=glm.vec3(0, 0, 1), start=glm.vec3(1, 0, 0),
                          inner=inner, outer=outer,
